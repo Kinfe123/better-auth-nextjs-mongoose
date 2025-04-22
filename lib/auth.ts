346f171de03b148a9450b557f80611d67d80e002
Kinfe123 <kinfishtech@gmail.com>
2025-04-22 18:32:20 +0300

    init

---
+++ lib/auth.ts
@@ -0,0 +1,50 @@
+import { APIError, betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+import { admin, createAuthMiddleware } from "better-auth/plugins";
+import { db } from "./db";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+export const auth = betterAuth({
+  database: mongodbAdapter(db),
+  emailAndPassword: {
+    enabled: true,
+  },
+  hooks: {
+    before: createAuthMiddleware(async (ctx) => {}),
+  },
+  account: {},
+  plugins: [admin(), nextCookies()],
+  databaseHooks: {
+    session: {
+      create: {
+        before: async (session) => {
+          console.log("session create", session);
+        },
+      },
+    },
+    user: {
+      update: {
+        before: async (session) => {
+          console.log("session update before", session, session);
+        },
+        after: async (session) => {
+          console.log("session update after", session);
+        },
+      },
+    },
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    twitter: {
+      clientId: process.env.TWITTER_CLIENT_ID!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+    },
+  },
+});
